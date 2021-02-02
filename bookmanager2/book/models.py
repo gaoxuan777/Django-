ea062c067c4db7338d3337bf420f213798b510f7
gaoxuan <1990937684@qq.com>
2021-02-02 19:31:35 +0800

    'day2'

--- bookmanager2/book/models.py
+++ bookmanager2/book/models.py
@@ -1,5 +1,33 @@
 from django.db import models
 
-class BookManger(models.Model):
 
 # Create your models here.
+class BookInfo(models.Model):
+    name=models.CharField(max_length=100)
+    pub_date=models.DateField(null=True)
+    readcount=models.IntegerField(default=0)
+    commentcount=models.IntegerField(default=0)
+    is_delete=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table='bookinfo'
+
+
+class PeopleInfo(models.Model):
+    gender_list=(
+        (1,'male'),
+        (2,'female')
+    )
+    name=models.CharField(max_length=10)
+    gender=models.SmallIntegerField(choices=gender_list,default=1)
+    description=models.CharField(max_length=1000)
+    is_delete=models.BooleanField(default=False)
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    class Meta:
+        db_table='peopleinfo'
+
+
--- bookmanager2/book/views.py
+++ bookmanager2/book/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from book.models import BookInfo
+BookInfo(
+    name='Django',
+    pub_date='2020-1-2',
+    readcount=0,
+    commentcount=10
+
+)
+BookInfo.objects.filter(pk=1).update(name='gao')
+BookInfo.objects.filter(pk=4).delete()
+BookInfo.objects.filter(name__contains='天')
+BookInfo.objects.filter(name__endswith='部')
+BookInfo.objects.filter(name__isnull=True)
+BookInfo.objects.filter(pk__in=[1,3,5])
+BookInfo.objects.filter(pk__gt=3)
+BookInfo.objects.filter(pub_date__year__gt=1980)
+from django.db.models import F
+BookInfo.objects.filter(commentcount__gt=F('readcount'))
+from django.db.models import Q
+BookInfo.objects.filter(~Q(id=3))
+BookInfo.objects.filter(Q(id=1)|Q(id=3))
+BookInfo.objects.aggregate(Sum('commentcount'))
+from django.db.models import Sum
